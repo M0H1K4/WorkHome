4d74bc7c557a22772838790626048cf774e6fe48
M0H1K4N <nejer911@gmail.com>
2024-01-08 20:30:43 +0400

    Add for no reason

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const fs  = require('fs');
+const express = require('express')
+const app = express();
+const port = 3000;
+
+
+app.get('/main', (req, res) => {
+    res.json()
+})
+
+
+app.listen(port, ()  =>{
+    console.log(`Listening on port ${port}`)
+    
+})
