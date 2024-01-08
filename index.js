42d5a9bf43d1692b62f62135dbd2c0343c301f77
M0H1K4N <nejer911@gmail.com>
2024-01-08 20:40:24 +0400

    Add

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 const port = 3000;
 
+const cars = [
+  { carNumber: 1, name: "Porsche", model: 911 },
+  { carNumber: 2, name: "Porsche", model: 911 },
+  { carNumber: 3, name: "Porsche", model: 911 },
+  { carNumber: 4, name: "Porsche", model: 911 },
+  { carNumber: 5, name: "Porsche", model: 911 },
+  { carNumber: 6, name: "Porsche", model: 911 },
+];
 
-app.get('/main', (req, res) => {
-    res.json()
-})
+const car1 = cars[0];
 
+app.get("/main", (req, res) => {
+  res.json(cars);
+});
 
-app.listen(port, ()  =>{
-    console.log(`Listening on port ${port}`)
-    
-})
+app.get("/main/:id", (req, res) => {
+  res.json(car1);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
