2e7e6e565570e3450d761aadcdc59c3f0778ed99
M0H1K4N <nejer911@gmail.com>
2024-01-08 21:26:44 +0400

    s

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+// const http = require("http");
+// const date = require("./myfirstmodule");
+
+// http
+//   .createServer(function (req, res) {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     res.write("The date and time currently is:" + date.myDateTime());
+//     res.end();
+//   })
+//   .listen(8080);
 const http = require("http");
-const date = require("./myfirstmodule");
 
 http
   .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("The date and time currently is:" + date.myDateTime());
+    res.write("Kodi kargad mushaobs");
     res.end();
   })
   .listen(8080);
