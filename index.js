9ea73b441b731ccac6b7b33d55777ee0bd895d76
M0H1K4N <nejer911@gmail.com>
2024-01-08 22:03:02 +0400

    Split the Query String

--- index.js
+++ index.js
@@ -19,11 +19,13 @@
 //   .listen(8080);
 
 const http = require("http");
+const url = require("url");
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(req.url);
-    res.end();
+    const q = url.parse(req.url, true).query;
+    const txt = q.year + " " + q.month;
+    res.end(txt);
   })
   .listen(8080);
