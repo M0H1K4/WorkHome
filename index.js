fffa20c44f68d57029149c3f8785f45873f76b26
M0H1K4N <nejer911@gmail.com>
2024-01-08 22:04:37 +0400

    AD

--- index.js
+++ index.js
@@ -19,13 +19,11 @@
 //   .listen(8080);
 
 const http = require("http");
-const url = require("url");
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    const q = url.parse(req.url, true).query;
-    const txt = q.year + " " + q.month;
-    res.end(txt);
+    res.write("Hello World!");
+    res.end();
   })
   .listen(8080);
