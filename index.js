899d2a96e341334c445620f2c21628e00445deab
M0H1K4N <nejer911@gmail.com>
2024-01-08 21:55:40 +0400

    add

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ const http = require("http");
 
 http
   .createServer(function (req, res) {
-    // res.writeHead(200, { "Content-Type": "text/html" });
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.write("Hello world");
-    res.end("json");
+    res.end();
   })
   .listen(8080);
