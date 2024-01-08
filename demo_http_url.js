82eb60851c8e9bf29ed05c90655555aaa691c8de
M0H1K4N <nejer911@gmail.com>
2024-01-08 22:06:49 +0400

    d

--- demo_http_url.js
+++ demo_http_url.js
@@ -1,11 +1,11 @@
-var http = require("http");
-var url = require("url");
+const http = require("http");
+const url = require("url");
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
+    const q = url.parse(req.url, true).query;
+    const txt = q.year + " " + q.month;
     res.end(txt);
   })
   .listen(8080);
