3732343984a6b10d77037a467bf828a73c05b1ca
M0H1K4N <nejer911@gmail.com>
2024-01-08 21:08:15 +0400

    adding my own module

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const http = require("http");
+const date = require("./myfirstmodule");
 
-const cars = [
-  { carNumber: 1, name: "Porsche", model: 911 },
-  { carNumber: 2, name: "Porsche", model: 911 },
-  { carNumber: 3, name: "Porsche", model: 911 },
-  { carNumber: 4, name: "Porsche", model: 911 },
-  { carNumber: 5, name: "Porsche", model: 911 },
-  { carNumber: 6, name: "Porsche", model: 911 },
-];
-
-const car1 = cars[0];
-
-app.get("/main", (req, res) => {
-  res.json(cars);
-});
-
-app.get("/main/:id", (req, res) => {
-  res.json(car1);
-});
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("The date and time currently is:" + date.myDateTime());
+    res.end();
+  })
+  .listen(8080);
